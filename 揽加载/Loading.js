19a1f6a242f4786a1805428cea86f4aed098622a
Spicy0strip <41494917+Spicy0strip@users.noreply.github.com>
2018-10-23 20:03:35 +0800

    Update 揽加载/Loading.js
    修改

--- 揽加载/Loading.js
+++ 揽加载/Loading.js
@@ -12,7 +12,36 @@ function lazy(imgg) {
     }
 }
 
+
+
 lazy(imgg);
 window.onscroll = function () {
-    lazy(imgg);
+    throttle(lazy(imgg), 10000);
+};
+
+//函数去抖
+function debounce(method, delay) {  //空闲时间大于或等于delay,method才会执行
+    var last = null;
+    return function () {
+        var content= this;
+        var args = arguments;
+        clearTimeout(last);
+        last = setTimeout(function () {
+            method.apply(content, args);
+        },delay);
+    }
+};
+
+//函数节流
+function throttle(method, delay) { //method执行频率限定在 次/delay
+
+    var last = 0;
+    return function () {
+        var curr = +new Date(); //返回毫秒数，相当于.valueOf(); .getTime()也是得到毫秒数
+        if (curr - last > delay) {
+            method.apply(this, arguments);
+            last = curr;
+        }
+    }
+
 };
